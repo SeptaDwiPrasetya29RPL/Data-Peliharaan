46b78c9c724b953a95ce7e345a547e3d542bae9e
SeptaDwiPrasetya29RPL <septa_dwi_29rpl@student.smktelkom-mlg.sch.id>
2021-01-27 21:01:36 +0700

    first commit

--- src/datapeliharaan/peliharaan.java
+++ src/datapeliharaan/peliharaan.java
@@ -4,20 +4,35 @@ package datapeliharaan;
 public class peliharaan {
     
     String nama;
+    String umur;
+    String jenis;
     String makanan;
     
     void Kucing(){
         nama = "Carberus";
+        jenis = "Kucing";
+        umur = "8 Bulan";
         makanan = "Daging";
-        System.out.println("Nama Kucing Saya Adalah "+nama);
-        System.out.println("Kucing Saya Suka Makan "+makanan);
+        System.out.println("==========PELIHARAAN 1==========");
+        System.out.println("================================");
+        System.out.println("Jenis Peliharaan : "+jenis);
+        System.out.println("Nama Peliharaan : "+nama);
+        System.out.println("Umur : "+umur);
+        System.out.println("Makanan : "+makanan);
+        
     }
     
     void Kambing(){
         nama = "Amaterasu";
+        jenis = "Kambing";
+        umur = "2 Tahun";
         makanan = "Rumput";
-        System.out.println("Nama Kambing Saya Adalah "+nama);
-        System.out.println("Kambing Saya Suka Makan "+makanan);
+        System.out.println("==========PELIHARAAN 2==========");
+        System.out.println("================================");
+        System.out.println("Jenis Peliharaan : "+jenis);
+        System.out.println("Nama Peliharaan : "+nama);
+        System.out.println("Umur : "+umur);
+        System.out.println("Makanan : "+makanan);
     }
     
 }
