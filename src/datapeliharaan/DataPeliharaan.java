fba84155e95c91d1e6ffdc5d03a3d6e4f2ad9e94
SeptaDwiPrasetya29RPL <septa_dwi_29rpl@student.smktelkom-mlg.sch.id>
2021-01-27 20:22:41 +0700

    first commit

---
+++ src/datapeliharaan/DataPeliharaan.java
@@ -0,0 +1,13 @@
+
+package datapeliharaan;
+
+public class DataPeliharaan {
+
+    public static void main(String[] args) {
+        
+        peliharaan hewan = new peliharaan();
+        hewan.Kucing();
+        hewan.Kambing();
+    }
+   
+}
---
+++ src/datapeliharaan/peliharaan.java
@@ -0,0 +1,23 @@
+
+package datapeliharaan;
+
+public class peliharaan {
+    
+    String nama;
+    String makanan;
+    
+    void Kucing(){
+        nama = "Carberus";
+        makanan = "Daging";
+        System.out.println("Nama Kucing Saya Adalah "+nama);
+        System.out.println("Kucing Saya Suka Makan "+makanan);
+    }
+    
+    void Kambing(){
+        nama = "Amaterasu";
+        makanan = "Rumput";
+        System.out.println("Nama Kambing Saya Adalah "+nama);
+        System.out.println("Kambing Saya Suka Makan "+makanan);
+    }
+    
+}
